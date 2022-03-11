e6a4183121af2b35b66cbb502ba197ee028914be
subham-santra <32807547+Subham1999@users.noreply.github.com>
2022-03-11 14:41:00 +0530

    Update PascalTriangle.java

--- recursion/PascalTriangle.java
+++ recursion/PascalTriangle.java
@@ -1,27 +1,36 @@
+package org.example.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
-** @see : <a href="https://leetcode.com/problems/pascals-triangle/">click here</a>
-*/
-class Solution {
-    public List<List<Integer>> generate(int numRows) {
-        if (numRows == 1)
-            return List.of(List.of(1));
-        List<List<Integer>> table = new ArrayList<>();
-        for (int i = 1; i <= numRows; ++i) {
-            table.add(new ArrayList<>());
-        }
-        
-        
-        for (int row = 1; row <= numRows; ++row) {
-            for (int col = 1; col <= row; ++col) {
-                if (col == 1 || col == row) {
-                    table.get(row - 1).add(1);
-                } else {
-                    table.get(row - 1).add(
-                        table.get(row - 1 - 1).get(col - 1 - 1) + table.get(row - 1 - 1).get(col - 1)
-                    );
+ * @see : <a href='https://leetcode.com/problems/pascals-triangle/'>click here</a>
+ * @author : Subham Santra
+ */
+public class PascalTriangle {
+
+    class Solution {
+        public List<List<Integer>> generate(int numRows) {
+            if (numRows == 1)
+                return List.of(List.of(1));
+            List<List<Integer>> table = new ArrayList<>();
+            for (int i = 1; i <= numRows; ++i) {
+                table.add(new ArrayList<>());
+            }
+
+
+            for (int row = 1; row <= numRows; ++row) {
+                for (int col = 1; col <= row; ++col) {
+                    if (col == 1 || col == row) {
+                        table.get(row - 1).add(1);
+                    } else {
+                        table.get(row - 1).add(
+                                table.get(row - 1 - 1).get(col - 1 - 1) + table.get(row - 1 - 1).get(col - 1)
+                        );
+                    }
                 }
             }
+            return table;
         }
-        return table;       
     }
 }
