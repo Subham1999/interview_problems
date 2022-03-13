c3132509c3f21dc7cdc9acdf67a35812ba7172ca
test <subhamsantra2016@gmail.com>
2022-03-13 10:21:21 +0530

    Two approaches

--- recursion/ReverseLinkedListII.java
+++ recursion/ReverseLinkedListII.java
@@ -1,3 +1,4 @@
+// #### kARTHIK
 class Solution {
     public ListNode findNthNode(ListNode node,int offset){
         // if left==1
@@ -38,5 +39,91 @@ class Solution {
         ListNode nextNode = currentNode.next;
         currentNode.next = prevNode;
         return func(nextNode, currentNode,right);
+    }
+}
+// #### subham
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class AnotherSolution {
+    void print(ListNode head) {
+        if (head == null) {
+            System.out.println("null");
+            return;
+        }
+        for (ListNode node = head; node != null; node = node.next) {
+            System.out.printf("%d ", node.val);
+        }
+        System.out.println();
+    }
+    ListNode nthNode(ListNode head, int n) {
+        if (n == 0)
+            return null;
+        ListNode node = head;
+        while (n > 0) {
+            node = node.next;
+            n--;
+        }
+        return node;
+    }
+    
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        // base case
+        if (left == right)
+            return head;
+        
+        ListNode leftNode = nthNode(head, left - 1);
+        ListNode rightNode = nthNode(head, right - 1);
+//         print(leftNode);
+//         print(rightNode);
+//         System.out.println();
+
+        ListNode nextLeft = head;
+        
+        if (leftNode == null) {
+            nextLeft = null;
+            leftNode = head;
+        } else {
+            while (nextLeft.next != leftNode) {
+                nextLeft = nextLeft.next;
+            }
+        }
+        
+        ListNode prev = rightNode == null ? null : rightNode.next;
+        if (rightNode != null) {
+            rightNode.next = null;
+        }
+        
+        ListNode ans = reverse(head, nextLeft, leftNode, prev);
+        return ans;
+    }
+    
+    ListNode reverse(ListNode head, ListNode beforeLeft, ListNode current, ListNode previous) {
+        // print(head);
+        // print(beforeLeft);
+        // print(current);
+        // print(previous);
+        // System.out.println("----------------------");
+        if (current == null) {
+            if (beforeLeft != null) {
+                beforeLeft.next = previous;
+                return head;
+            } else {
+                return previous;
+            }
+        }
+        
+        ListNode next = current.next;
+        current.next = previous;
+        previous = current;
+        current = next;
+        return reverse(head, beforeLeft, current, previous);
     }
 }
