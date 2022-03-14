a6304db1b95d12516044052aebc4f828b866d59a
karthikk2505 <55589994+karthikk2505@users.noreply.github.com>
2022-03-14 17:33:50 +0530

    Update CombinationSum.java

--- recursion/CombinationSum.java
+++ recursion/CombinationSum.java
@@ -48,3 +48,30 @@ public class CombinationSum {
         }
     }
 }
+
+
+//Karthik
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    public void generate(int[] candidates,int n,int target,int k,List<Integer> cur){
+        if(target<0){
+            return;
+        }
+        if(target==0){
+            res.add(new ArrayList<>(cur));
+        }
+        else{
+            for(int i=k;i<n;i++){
+                cur.add(candidates[i]);
+                generate(candidates,n,target-candidates[i],i,cur);
+                cur.remove(new Integer(candidates[i]));
+            }
+        }
+    }
+    //- f([2,3,6,7],7)
+     //   -f()
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        generate(candidates, candidates.length,target,0,new ArrayList<>());
+        return res;
+    }
+}
